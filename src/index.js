eb555fc676877780ebe4e079e94bb3b09dfa9318
abhi0661236 <ap0661236@gmail.com>
2021-07-15 17:11:51 +0530

    Added game board in project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component
+{   
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null,
+        }
+    }
+
+    render()
+    {   
+        return (
+            <button 
+            className = "square" 
+            onClick = {() => this.setState({value:'X'})}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component 
+{
+    renderSqaure(i)
+    {
+        return <Square value = {i}/>;
+    }
+
+    render() 
+    {
+        const status = 'Next player: X';
+        
+        return (
+            <div>
+                <div className = "status">
+                    {status}
+                </div>
+                <div className = "board-row">
+                    {this.renderSqaure(0)}
+                    {this.renderSqaure(1)}
+                    {this.renderSqaure(2)}
+                </div>
+                <div className = "board-row">
+                    {this.renderSqaure(3)}
+                    {this.renderSqaure(4)}
+                    {this.renderSqaure(5)}
+                </div>
+                <div className = "board-row">
+                    {this.renderSqaure(6)}
+                    {this.renderSqaure(7)}
+                    {this.renderSqaure(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component{
+    render(){
+        return(
+            <div className = "game">
+                <div className = "game-board">
+                    <Board />
+                </div>
+                <div className = "game-info">
+                    <div>{/* status */}</div>
+                    <div>{/* TODO */}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
